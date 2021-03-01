98a4ad09d4d3cd82c3823c3f8336d396fa610336
nishiwakki <glotply@gmail.com>
2021-03-01 23:06:02 +0900

    2021/3/1

---
+++ dpl/knapsack_problem.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+
+# N, W入力
+N, W = map(int, input().split())
+# DPを0で初期化
+dp = [[0] * (W+1) for _ in range(N+1)]
+# 各品物について
+for i in range(1, N+1):
+    # 品物入力
+    v, w = map(int, input().split())
+    # 各重さについて(貰うDP)
+    for j in range(1, W+1):
+        if j-w >= 0:
+            # i-1, i からの遷移
+            dp[i][j] = max(dp[i-1][j], dp[i][j-w]+v)
+        else:
+            dp[i][j] = dp[i-1][j]
+    print(dp[i])
+# 出力
+print(dp[N][W])
